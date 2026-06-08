6eedf1682fb1c9f76ef22ec378950a8ec4b1e0ac
rnicolas <nicolas.royer@ynov.com>
2026-06-08 15:39:05 +0200

    feat(menu): added a menu

--- Hangman.c
+++ Hangman.c
@@ -150,6 +150,39 @@
 
 // Partie de nicolas GRAAAOOOUU
 
+void printmenu () {
+printf(" ==== Menu ==== ");
+printf("1. Choisir le mot ");
+printf("2. Lancer une partie ");
+printf("3. Quitter le jeu ");
+printf(" Votre choix : ")       
+}
+
+int main () {
+    word [0] = '\0';
+    int choise;
+
+    do {
+        printmenu();
+        scanf("%d",&choise);
+
+        switch (choice)
+        {
+        case 1:
+            chooseWord();
+            break;
+        case 2: 
+            launchGame();
+            break;
+        case 3:
+            printf("envoie un message quand t'es rentrer !")
+        default:
+            printf("écrit mieux")
+            break;
+        } while (choise != 3);
+    }
+    return 0
+}
 
 
 
